c5bfa3826bc59179f7029fd38a3f97ca807be046
reifred <reifred33@gmail.com>
2018-12-31 05:27:35 +0300

    Add create red flag route

---
+++ app/models.py
@@ -0,0 +1,81 @@
+import random
+
+
+class Incident:
+
+    incident_id = 1
+
+    def __init__(
+            self, createdBy, createdOn, type, location, status, images,
+             videos, comment):
+        self._id = Incident.incident_id
+        self.createdBy = createdBy
+        self.createdOn = createdOn
+        self.type = type
+        self.location = location
+        self.status = status
+        self.images = images
+        self.videos = videos
+        self.comment = comment
+        Incident.incident_id += 1
+
+    def validate_input(self):
+        if not self.createdBy or not isinstance(self.createdBy, int):
+            return "createdBy must be an int"
+        if not self.location or not isinstance(
+                self.location, str) or self.location.isspace():
+            return "location must not be empty string"
+        if not self.status or not isinstance(
+                self.status, str) or self.status.isspace():
+            return "status must not be empty string"
+        if not self.comment or not isinstance(
+                self.comment, str) or self.comment.isspace():
+            return "comment must not be empty string"
+        if not self.type or not isinstance(
+                self.type, str) or self.type.isspace():
+            return "type must not be empty string"
+        if not self.images:
+            return "images must not be empty"
+
+
+    @staticmethod
+    def validate_location(location):
+        if not location or not isinstance(location, str) or location.isspace():
+            return "location must not be empty string"
+
+    @staticmethod
+    def validate_comment(comment):
+        if not comment or not isinstance(comment, str) or comment.isspace():
+            return "comment must not be empty string"
+
+    def convert_to_dict(self):
+        return dict(id=self._id, createdOn=self.createdOn,
+                    type=self.type, location=self.location,
+                    status=self.status, images=[self.images],
+                    videos=[self.videos], comment=self.comment)
+
+
+class User:
+
+    user_id = 1
+
+    def __init__(self,firstname,lastname,othernames,email,phoneNumber,
+    username,registered,isAdmin):
+        self.id = id(User.user_id)
+        self.firstname = firstname
+        self.lastname = lastname
+        self.othernames = othernames
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self.username = username
+        self.registered = registered
+        self.isAdmin = isAdmin
+        User.user_id += 1
+
+    def convert_to_dict(self):
+        return dict(id=self.id, firstname=self.firstname,
+                    lastname=self.lastname, othernames=self.othernames,
+                    email=self.email, phoneNumber=self.phoneNumber,
+                    username=self.username, registered=self.registered,
+                    isAdmin=self.isAdmin)
+
---
+++ app/routes.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify,make_response, request
+from app.models import Incident
+from datetime import datetime
+
+app = Flask(__name__)
+
+red_flags = []
+
+
+@app.route("/")
+def index():
+    return "Welcome to api."
+
+
+@app.route("/api/v1/red_flags", methods=["POST"])
+def create_red_flag_record():
+    """Create a red flag"""
+    red_flag = request.get_json()
+    if not request.is_json:
+        return jsonify({
+            "status": 400,
+            "error": "JSON request required"
+        }), 400
+    createdOn = datetime.now().strftime("%Y-%m-%d")
+    createdBy = red_flag.get("createdBy")
+    status = red_flag.get("status")
+    comment = red_flag.get("comment")
+    type = red_flag.get("type")
+    images = red_flag.get("images")
+    videos = red_flag.get("videos")
+    location = red_flag.get("location")
+
+    incident = Incident(
+        createdBy,createdOn,type,location,status,images,videos,comment)
+    errors = incident.validate_input()
+    if errors:
+        return jsonify({"status":400,"error":errors}),400
+
+    red_flags.append(incident.convert_to_dict())
+    
+    return jsonify({
+        "status": 201,
+        "data": [{
+            "id": incident._id,
+            "message": "Created red-flag record"}]
+    }), 201
